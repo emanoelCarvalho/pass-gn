801c544685716eaae2710d7aeba49883db759a2e
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-05-02 14:16:22 -0300

    :bug: fix(App): implement theme switching based on color scheme

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { DarkTheme, DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { useColorScheme } from "react-native";
+
 import {Routes} from './src/routes'
 
 export default function App() {
+  const sheme = useColorScheme();
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={sheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Routes/>
     </NavigationContainer>
   )
