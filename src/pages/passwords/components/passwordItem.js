60ccc1ca753a8676222978f3d102e376e0b129d3
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-05-02 14:22:35 -0300

    :bug: fix(passwordItem): implement color scheme support for text in PasswordItem component

--- src/pages/passwords/components/passwordItem.js
+++ src/pages/passwords/components/passwordItem.js
@@ -1,28 +1,31 @@
 import React from "react";
-import { Text, StyleSheet, Pressable } from "react-native";
+import { Text, StyleSheet, Pressable, useColorScheme } from "react-native";
 
-export function PasswordItem({data, removePassword}) {
-    return (
-        <Pressable onLongPress={removePassword} style={StyleSheet.container}>
-            <Text style={styles.text}>
-                { data }
-            </Text>
-        </Pressable>
-    ); 
+export function PasswordItem({ data, removePassword }) {
+  const colorScheme = useColorScheme();
+  const isDarkMode = colorScheme === 'dark';
+
+  return (
+    <Pressable onLongPress={removePassword} style={styles.container}>
+      <Text style={[styles.text, { color: isDarkMode ? '#fff' : '#000' }]}>
+        {data}
+      </Text>
+    </Pressable>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#0e0e0e",
-        padding: 14,
-        width: "100%",
-        marginBottom: 14,
-        borderRadius: 8,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-    }, 
-    text: {
-        color: "#000"
-    }
-})
+  container: {
+    backgroundColor: "#0e0e0e",
+    padding: 14,
+    width: "100%",
+    marginBottom: 14,
+    borderRadius: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  text: {
+    fontSize: 16,
+  }
+});
